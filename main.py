df7f82736bc947ea3ce600ffe11ac2234f787064
yousufbilal <64967817+yousufbilal@users.noreply.github.com>
2026-05-25 16:15:25 +0100

    connection to cloud trail

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 from tools.cloudtrail_reader import get_security_events
 
 def run():
-    events = get_security_events()
-    print(events)
+    get_security_events()
 
 run()
-=======
->>>>>>> 5ca1b4a1eb57b77e04297450de3351a1fdb6ba80
--- tools/cloudtrail_reader.py
+++ tools/cloudtrail_reader.py
@@ -4,21 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_security_events():
+    client = boto3.client('cloudtrail')
+    response = client.lookup_events()
 
-    #connect to AWS CloudTrail
-    client = boto3.client('cloudtrail', region_name='eu-north-1')
-    
-    response = client.lookup_events(
-        LookupAttributes=[
-            {
-                'AttributeKey': 'ReadOnly',
-                'AttributeValue': 'false'
-            }
-        ],
-        MaxResults=20
-    )
-    
-    return response['Events']
-
-
-  
+    for events in response['Events']:
+        print(events['EventTime'], events['EventName'])
