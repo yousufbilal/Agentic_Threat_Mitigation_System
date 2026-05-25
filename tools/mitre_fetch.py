d1826197f4eacae855a941be34aca3034594f8ba
yousufbilal <64967817+yousufbilal@users.noreply.github.com>
2026-05-25 21:58:47 +0100

    IaaS metere directory itteration

--- tools/mitre_fetch.py
+++ tools/mitre_fetch.py
@@ -4,7 +4,6 @@ import requests
 def fetch_mitre_techniques():
     # Fetch MITRE ATT&CK techniques from the official repository.
     response = requests.get("https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json")
-    print(response.status_code)
     data = response.json()
 
     techniques = []
@@ -13,7 +12,14 @@ def fetch_mitre_techniques():
     for obj in data['objects']:
          if obj['type'] == 'attack-pattern':
              techniques.append(obj)
-            #  print(len(techniques))
-             print(techniques[0])
-    
-    return techniques
+
+# Filter techniques relevant to cloud 
+
+
+    for technique in techniques:
+        if 'IaaS' in technique.get('x_mitre_platforms', []):    
+            print(technique['name'],technique['description'])
+            # print(len(technique['name']))
+            return techniques
+
+
