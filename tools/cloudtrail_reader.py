0f351e7c110b17b634d22b2e527f13d3adb3e216
yousufbilal <64967817+yousufbilal@users.noreply.github.com>
2026-06-10 12:45:29 +0500

    added dummpy data

--- tools/cloudtrail_reader.py
+++ tools/cloudtrail_reader.py
@@ -1,24 +1,62 @@
 import boto3
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
-# This function retrieves security events from AWS CloudTrail.
+
+TRUSTED_SOURCES = [
+    "config.amazonaws.com",
+    "cloudtrail.amazonaws.com",
+    "health.amazonaws.com",
+    "resource-explorer-2.amazonaws.com"
+]
+
+HIGH_RISK_APIS = [
+    "AssumeRole",
+    "GetSecretValue",
+    "DeleteTrail",
+    "PutBucketPolicy",
+    "CreateUser",
+    "AttachUserPolicy",
+    "DeleteUser",
+    "CreateAccessKey",
+    "PutUserPolicy",
+    "AddUserToGroup"
+]
+
+def is_suspicious(event):
+    source_ip = event.get("sourceIPAddress", "")
+    event_name = event.get("eventName", "")
+
+    if source_ip in TRUSTED_SOURCES:
+        return False
+
+    if event_name in HIGH_RISK_APIS:
+        print(f"Suspicious event detected: {event_name} from {source_ip}")
+        return True
+
+    return False
+
 def get_security_events():
     client = boto3.client('cloudtrail')
-    response = client.lookup_events(MaxResults=5)
+    response = client.lookup_events(MaxResults=50)
 
     events = []
 
-# Iterate through the events and extract relevant information.
     for event in response['Events']:
-     events.append({
-            "event_time": str(event['EventTime']),
-            "event_name": event['EventName'],
-            "event_source": event['EventSource'],
-            "username": event.get('Username', 'N/A'),
-            "source_ip": event.get('SourceIPAddress', 'N/A'),
-            "error_code": event.get('ErrorCode', None),
-            "request_parameters": event.get('RequestParameters', {})
-        })
-     
-     return events
+        raw = json.loads(event['CloudTrailEvent'])
+
+        source_ip = raw.get('sourceIPAddress', 'N/A')
+        event_name = event['EventName']
+
+        if is_suspicious({"sourceIPAddress": source_ip, "eventName": event_name}):
+            events.append({
+                "event_time": str(event['EventTime']),
+                "event_name": event_name,
+                "event_source": event['EventSource'],
+                "username": event.get('Username', 'N/A'),
+                "source_ip": source_ip,
+                "error_code": raw.get('errorCode', None),
+                "request_parameters": raw.get('requestParameters', {})
+            })
+    return events
