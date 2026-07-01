9cfb1f9d821adc7c67af7529450177d59a78d251
yousufbilal <64967817+yousufbilal@users.noreply.github.com>
2026-07-01 13:07:52 +0100

    Build CloudTrail log reader using boto3

--- tools/cloudtrail_reader.py
+++ tools/cloudtrail_reader.py
@@ -1,33 +1,16 @@
 import boto3
-import json
 from dotenv import load_dotenv
-from Temp_Events.Events import events
-from cloud_trail_filter import is_suspicious
 
 load_dotenv()
 
+def get_security_events():
+    client = boto3.client('cloudtrail')
+    response = client.lookup_events(MaxResults=50)
 
+    print("CloudTrail Events:", response['Events'])
 
-# def get_security_events():
-#     client = boto3.client('cloudtrail')
-#     response = client.lookup_events(MaxResults=50)
+    return response['Events']
 
-#     events = []
-
-#     for event in response['Events']:
-#         raw = json.loads(event['CloudTrailEvent'])
-
-#         source_ip = raw.get('sourceIPAddress', 'N/A')
-#         event_name = event['EventName']
-
-#         if is_suspicious({"sourceIPAddress": source_ip, "eventName": event_name}):
-#             events.append({
-#                 "event_time": str(event['EventTime']),
-#                 "event_name": event_name,
-#                 "event_source": event['EventSource'],
-#                 "username": event.get('Username', 'N/A'),
-#                 "source_ip": source_ip,
-#                 "error_code": raw.get('errorCode', None),
-#                 "request_parameters": raw.get('requestParameters', {})
-#             })
-#     return events
+if __name__ == "__main__":
+    events = get_security_events()
+    print(events)
